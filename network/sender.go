90ed16fe94ade8a5cafedbf146c727f5f9ddca01
ganluo <gan.luo@igg.com>
2021-11-30 18:09:07 +0800

    update

--- network/sender.go
+++ network/sender.go
@@ -12,6 +12,7 @@ type MsgSender struct {
 	msgList    chan INetMsg
 }
 
+// 异步发送数据
 func (ms *MsgSender) Send(msg INetMsg) error {
 	if msg == nil || ms.conn == nil || ms.conn.IsClosed() {
 		return nil
@@ -25,4 +26,42 @@ func (ms *MsgSender) Send(msg INetMsg) error {
 		logger.LogWarnf("%v", err)
 		return err
 	}
+}
+
+// 同步发送数据
+func (ms *MsgSender) SyncSend(msg INetMsg) (err error) {
+	return ms.rawSend(msg)
+}
+
+func (ms *MsgSender) rawSend(msg INetMsg) error {
+	if msg == nil || ms.conn == nil || ms.conn.IsClosed() {
+		return nil
+	}
+
+	_, err := ms.conn.Write(msg.Encode())
+	return err
+}
+
+// 发送线程启动
+func (ms *MsgSender) start() {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				logger.LogErrf("Sender %v error: %v.", ms, r)
+			}
+		}()
+
+		for {
+			select {
+			case msg := <-ms.msgList:
+				if err := ms.rawSend(msg); err != nil {
+					logger.LogErrf("send message %v: %v", msg, err)
+				}
+
+			case <-ms.exitSignal:
+				logger.Logf("Sender close: %p %v.", ms, ms)
+				return
+			}
+		}
+	}()
 }
