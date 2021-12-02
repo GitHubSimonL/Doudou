402a17549b13c05785761796def975e9341ddb42
ganluo <gan.luo@igg.com>
2021-12-02 15:49:50 +0800

    update

--- framework/itr/iserver.go
+++ framework/itr/iserver.go
@@ -5,9 +5,9 @@ type IServer interface {
 	Stop()                                       // 停止
 	SetHandler(msgID int32, handle IHandle)      // 根据MsgID设置handle方法
 	GetConnMgr() IConnMgr                        // 获取server所有链接管理器
-	SetConnStartHookFunc(func(conn IConnection)) // 链接创建时的hood方法
-	CallConnStartHookFunc(conn IConnection)      // 调用链接创建hood方法
-	SetConnEndHookFunc(func(conn IConnection))   // 链接断开时的hood方法
-	CallConnEndHookFunc(conn IConnection)        // 调用链接断开hood方法
+	SetConnStartHookFunc(func(conn IConnection)) // 链接创建时的hoo方法
+	CallConnStartHookFunc(conn IConnection)      // 调用链接创建hoo方法
+	SetConnEndHookFunc(func(conn IConnection))   // 链接断开时的hoo方法
+	CallConnEndHookFunc(conn IConnection)        // 调用链接断开hook方法
 	Packet() IPacket                             // 数据打包与解包对象
 }
