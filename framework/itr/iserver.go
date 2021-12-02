6694db8bfab0da664605c75e9d5a0a96da9db48d
ganluo <gan.luo@igg.com>
2021-12-02 17:54:26 +0800

    update

--- framework/itr/iserver.go
+++ framework/itr/iserver.go
@@ -3,19 +3,20 @@ package itr
 type Option func(server IServer)
 
 type IServer interface {
-	Start()                                      // 启动服务器
-	Stop()                                       // 停止
-	SetHandler(msgID int32, handle IHandle)      // 根据MsgID设置handle方法
-	GetConnMgr() IConnMgr                        // 获取server所有链接管理器
-	SetConnStartHookFunc(func(conn IConnection)) // 链接创建时的hook方法
-	CallConnStartHookFunc(conn IConnection)      // 调用链接创建hook方法
-	SetConnEndHookFunc(func(conn IConnection))   // 链接断开时的hook方法
-	CallConnEndHookFunc(conn IConnection)        // 调用链接断开hook方法
-	Packet() IPacket                             // 数据打包与解包对象
-	SetType(svrType int32)                       // 设置类型
-	SetID(svrID int32)                           // 设置ID
-	SetIP(ip string)                             // 设置IP
-	SetPort(port int)                            // 设置端口
+	Start()                                                            // 启动服务器
+	Stop()                                                             // 停止
+	SetHandler(msgID uint32, handle IHandle)                           // 根据MsgID设置handle方法
+	GetConnMgr() IConnMgr                                              // 获取server所有链接管理器
+	GetPacket() IPacket                                                // 数据打包与解包对象
+	SetPacket(IPacket)                                                 // 设置数据打包与解包对象
+	SetType(svrType int32)                                             // 设置类型
+	SetID(svrID int32)                                                 // 设置ID
+	SetIP(ip string)                                                   // 设置IP
+	SetPort(port int)                                                  // 设置端口
+	SetMsgHandlerMgr(mgr IHandleMgr)                                   // 设置协议处理器
+	SetConnectHookFunc(connected, disConnected func(conn IConnection)) // 设置网络连接方法
+	CallConnStartHookFunc(conn IConnection)                            // 调用链接创建hook方法
+	CallConnEndHookFunc(conn IConnection)                              // 调用链接断开hook方法
 }
 
 func WithSvrType(svrType int32) Option {
@@ -40,4 +41,112 @@ func WithPort(port int) Option {
 	return func(server IServer) {
 		server.SetPort(port)
 	}
+}
+
+func WithPacket(packet IPacket) Option {
+	return func(server IServer) {
+		server.SetPacket(packet)
+	}
+}
+
+func WithConnHookFunc(connected, disconnected func(conn IConnection)) Option {
+	return func(server IServer) {
+		server.SetConnectHookFunc(connected, disconnected)
+	}
+}
+
+// server 基类实现
+type BaseServer struct {
+	svrType            int32
+	sveID              int32
+	ip                 string
+	port               int
+	onConnConnected    func(conn IConnection) // 简历链接hookFunc
+	onConnDisconnected func(conn IConnection) // 断开链接hookFunc
+	packet             IPacket                // 封包解包管理
+	msgHandler         IHandleMgr             // 协议处理管理器
+	connMgr            IConnMgr               // 链接管理器
+}
+
+var _ IServer = (*BaseServer)(nil)
+
+func NewBaseServer() *BaseServer {
+	return &BaseServer{}
+}
+
+func (b *BaseServer) Start() {
+	panic("implement me")
+}
+
+func (b *BaseServer) Stop() {
+	panic("implement me")
+}
+
+func (b *BaseServer) SetHandler(msgID uint32, handle IHandle) {
+	if b.msgHandler == nil {
+		return
+	}
+
+	b.msgHandler.RegisterHandle(msgID, handle)
+}
+
+func (b *BaseServer) GetConnMgr() IConnMgr {
+	return b.connMgr
+}
+
+func (b *BaseServer) GetPacket() IPacket {
+	return b.packet
+}
+
+func (b *BaseServer) SetPacket(packet IPacket) {
+	if packet == nil {
+		return
+	}
+
+	b.packet = packet
+}
+
+func (b *BaseServer) SetType(svrType int32) {
+	b.svrType = svrType
+}
+
+func (b *BaseServer) SetID(svrID int32) {
+	b.sveID = svrID
+}
+
+func (b *BaseServer) SetIP(ip string) {
+	b.ip = ip
+}
+
+func (b *BaseServer) SetPort(port int) {
+	b.port = port
+}
+
+func (b *BaseServer) SetMsgHandlerMgr(mgr IHandleMgr) {
+	if mgr == nil {
+		return
+	}
+
+	b.msgHandler = mgr
+}
+
+func (b *BaseServer) SetConnectHookFunc(connected, disConnected func(conn IConnection)) {
+	b.onConnConnected = connected
+	b.onConnDisconnected = disConnected
+}
+
+func (b *BaseServer) CallConnStartHookFunc(conn IConnection) {
+	if b.onConnConnected == nil {
+		return
+	}
+
+	b.onConnConnected(conn)
+}
+
+func (b *BaseServer) CallConnEndHookFunc(conn IConnection) {
+	if b.onConnDisconnected == nil {
+		return
+	}
+
+	b.onConnDisconnected(conn)
 }
--- framework/network/tcpserver.go
+++ framework/network/tcpserver.go
@@ -3,22 +3,19 @@ package network
 import "Doudou/framework/itr"
 
 type TcpServer struct {
-	svrType            int32
-	sveID              int32
-	ip                 string
-	port               int
-	onConnConnected    func(conn itr.IConnection) // 简历链接hookFunc
-	onConnDisconnected func(conn itr.IConnection) // 断开链接hookFunc
-	packet             itr.IPacket                // 封包解包管理
-	msgHandler         itr.IHandleMgr             // 协议处理管理器
-	connMgr            itr.IConnMgr               // 链接管理器
+	*itr.BaseServer
 }
 
 var _ itr.IServer = (*TcpServer)(nil)
 
 func NewTcpServer(ops ...itr.Option) itr.IServer {
 	server := &TcpServer{
-		packet: NewNetPacket(),
+		BaseServer: itr.NewBaseServer(),
+	}
+
+	server.SetPacket(NewNetPacket())
+	for _, op := range ops {
+		op(server)
 	}
 
 	return server
@@ -30,48 +27,4 @@ func (t *TcpServer) Start() {
 
 func (t *TcpServer) Stop() {
 	panic("implement me")
-}
-
-func (t *TcpServer) SetHandler(msgID int32, handle itr.IHandle) {
-	panic("implement me")
-}
-
-func (t *TcpServer) GetConnMgr() itr.IConnMgr {
-	panic("implement me")
-}
-
-func (t *TcpServer) SetConnStartHookFunc(f func(conn itr.IConnection)) {
-	panic("implement me")
-}
-
-func (t *TcpServer) CallConnStartHookFunc(conn itr.IConnection) {
-	panic("implement me")
-}
-
-func (t *TcpServer) SetConnEndHookFunc(f func(conn itr.IConnection)) {
-	panic("implement me")
-}
-
-func (t *TcpServer) CallConnEndHookFunc(conn itr.IConnection) {
-	panic("implement me")
-}
-
-func (t *TcpServer) Packet() itr.IPacket {
-	panic("implement me")
-}
-
-func (t *TcpServer) SetIP(ip string) {
-	t.ip = ip
-}
-
-func (t *TcpServer) SetPort(port int) {
-	t.port = port
-}
-
-func (t *TcpServer) SetType(svrType int32) {
-	t.svrType = svrType
-}
-
-func (t *TcpServer) SetID(svrID int32) {
-	t.sveID = svrID
 }
