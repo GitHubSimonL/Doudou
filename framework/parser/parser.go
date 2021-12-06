1b00ea71099b6954bca0f5796d22b057e5ffefc7
ganluo <gan.luo@igg.com>
2021-12-06 16:02:14 +0800

    update

--- framework/parser/parser.go
+++ framework/parser/parser.go
@@ -5,37 +5,37 @@ import (
 	"Doudou/lib/logger"
 	"fmt"
 	"github.com/golang/protobuf/proto"
-	"google.golang.org/protobuf/reflect/protoreflect"
+	"reflect"
 )
 
 // protobuf消息解析器
 type Processor struct {
-	id2type map[uint32]protoreflect.MessageType // id to type映射
-	type2id map[protoreflect.MessageType]uint32 // type to id映射
+	id2type map[uint32]reflect.Type // id to type映射
+	type2id map[reflect.Type]uint32 // type to id映射
 }
 
 func NewProcessor() *Processor {
 	processor := new(Processor)
-	processor.type2id = make(map[protoreflect.MessageType]uint32)
-	processor.id2type = make(map[uint32]protoreflect.MessageType)
+	processor.type2id = make(map[reflect.Type]uint32)
+	processor.id2type = make(map[uint32]reflect.Type)
 	return processor
 }
 
 // 注册消息类型
 func (p *Processor) Register(msg proto.Message) {
-	reflect := proto.MessageReflect(msg)
-	msgID := bkdrhash.BKDRHash(string(reflect.Descriptor().Name()))
+	msgType := reflect.TypeOf(msg)
+	msgID := bkdrhash.BKDRHash(msgType.Elem().Name())
 	// 类型必须不为空，且为指针
-	if reflect.Type() == nil {
+	if msgType == nil || msgType.Kind() != reflect.Ptr {
 		logger.LogFatal("require protobuf message pointer")
 	}
 	// 不能重复注册
-	if _, ok := p.type2id[reflect.Type()]; ok {
-		logger.LogFatalf("message %s is already registered", reflect.Type())
+	if _, ok := p.type2id[msgType]; ok {
+		logger.LogFatalf("message %s is already registered", msgType)
 	}
 	// 类型和ID双向注册
-	p.type2id[reflect.Type()] = msgID
-	p.id2type[msgID] = reflect.Type()
+	p.type2id[msgType] = msgID
+	p.id2type[msgID] = msgType
 }
 
 func (p *Processor) Unmarshal(msgID uint32, data []byte) (proto.Message, error) {
@@ -45,18 +45,18 @@ func (p *Processor) Unmarshal(msgID uint32, data []byte) (proto.Message, error) 
 		return nil, fmt.Errorf("message id %v not registered", msgID)
 	}
 	// 根据类型反序列化
-	msg := proto.MessageV1(typ.New())
+	msg := reflect.New(typ.Elem()).Interface().(proto.Message)
 	err := proto.Unmarshal(data, msg)
 	return msg, err
 }
 
 // Marshal 序列化
 func (p *Processor) Marshal(msg proto.Message) (uint32, []byte, error) {
-	reflect := proto.MessageReflect(msg)
+	msgType := reflect.TypeOf(msg)
 	// 根据类型查找消息id
-	msgId, ok := p.type2id[reflect.Type()]
+	msgId, ok := p.type2id[msgType]
 	if !ok {
-		err := fmt.Errorf("msg %s not registered", reflect.Type())
+		err := fmt.Errorf("msg %s not registered", msgType)
 		return 0, nil, err
 	}
 	// 序列化
@@ -68,17 +68,15 @@ func (p *Processor) Marshal(msg proto.Message) (uint32, []byte, error) {
 }
 
 func (p *Processor) GetMsgID(msg proto.Message) uint32 {
-	reflect := proto.MessageReflect(msg)
-
-	return p.type2id[reflect.Type()]
+	return p.type2id[reflect.TypeOf(msg)]
 }
 
-func (p *Processor) GetMsgType(msgID uint32) protoreflect.MessageType {
+func (p *Processor) GetMsgType(msgID uint32) reflect.Type {
 	return p.id2type[msgID]
 }
 
 // 迭代调用
-func (p *Processor) Range(f func(id uint32, t protoreflect.MessageType)) {
+func (p *Processor) Range(f func(id uint32, t reflect.Type)) {
 	for id, typ := range p.id2type {
 		f(id, typ)
 	}
